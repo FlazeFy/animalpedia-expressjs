fb0028b523904c198789b546bbace124975016e9
Leonardho R Sitanggang <79736302+FlazeFy@users.noreply.github.com>
2024-02-10 21:40:09 +0700

    get all news header

--- animalpedia/modules/animals/http_handlers/http_handlers.js
+++ animalpedia/modules/animals/http_handlers/http_handlers.js
@@ -10,4 +10,12 @@ router.get('/api/v1/animal', (req, res) => {
     repoQuery.getAllAnimals(req, res, '/api/v1/animal', page, pageSize)
 })
 
+// Get All News Headers
+router.get('/api/v1/news', (req, res) => {
+    const page = parseInt(req.query.page)
+    const pageSize = 10
+
+    repoQuery.getAllNews(req, res, '/api/v1/news', page, pageSize)
+})
+
 module.exports = router
--- animalpedia/modules/animals/repositories/queries.js
+++ animalpedia/modules/animals/repositories/queries.js
@@ -3,14 +3,14 @@ const generateQueryMsg = require('../../../packages/helpers/generator')
 const buildPaginationResponse = require('../../../packages/utils/pagination/services')
 const builders = require('../../../packages/builders/template')
 
-const baseTable = 'animals'
-
 function getAllAnimals(req, res, path, page, pageSize){
     // Query Builder
+    const baseTable = 'animals'
+
     selectTemplate = builders.getTemplateSelect("content_info", baseTable, null)
 	activeTemplate = builders.getTemplateLogic("active")
 	order = builders.getTemplateOrder("dynamic_data", baseTable, "animals_name")
-
+    
     const offset = (page - 1) * pageSize
     const sqlStatement = "SELECT " + selectTemplate + ", animals_latin_name, animals_img_url, animals_region, animals_zone, animals_status, animals_category " +
         "FROM " + baseTable + " " +
@@ -56,6 +56,61 @@ function getAllAnimals(req, res, path, page, pageSize){
     })
 }
 
+function getAllNews(req, res, path, page, pageSize){
+    // Query Builder
+    const baseTable = 'news'
+
+    selectTemplate = builders.getTemplateSelect("content_info", baseTable, null)
+    propsTemplate = builders.getTemplateSelect("properties_time", null, null)
+	activeTemplate = builders.getTemplateLogic("active")
+	order = builders.getTemplateOrder("dynamic_data", baseTable, "news_name")
+
+    const offset = (page - 1) * pageSize
+    const sqlStatement = "SELECT " + selectTemplate + ", news_tag, news_body, news_time_read, news_img_url, " + propsTemplate + " " +
+        "FROM " + baseTable + " " +
+        "WHERE " + activeTemplate + " " +
+        "ORDER BY " + order + " " +
+        "LIMIT "+pageSize+" OFFSET "+offset
+
+    connection.query(sqlStatement, (err, rows, fields) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            // Page
+            const total = rows.length
+            const totalPages = Math.ceil(total / pageSize);
+            const pagination = buildPaginationResponse(page, pageSize, total, totalPages, path)
+
+            // Response
+            let rowsFinal = rows
+            if(total != 0){
+                rowsFinal = {
+                    "current_page": page,
+                    "data": rows,
+                    "first_page_url": pagination.first_page_url,
+                    "from": pagination.from,
+                    "last_page": pagination.last_page,
+                    "last_page_url": pagination.last_page_url,
+                    "links": pagination.links,
+                    "next_page_url": pagination.next_page_url,
+                    "path": pagination.path,
+                    "per_page": pageSize,
+                    "prev_page_url": pagination.prev_page_url,
+                    "to": pagination.to,
+                    "total": total,
+                }
+            }
+            
+            res.status(200).json({ 
+                message: generateQueryMsg(baseTable,total), 
+                status: 200, 
+                data: rowsFinal 
+            })
+        }
+    })
+}
+
 module.exports = {
-    getAllAnimals
+    getAllAnimals,
+    getAllNews
 }
